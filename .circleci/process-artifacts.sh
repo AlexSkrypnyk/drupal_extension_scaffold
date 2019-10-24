4f9e4a0d288bbb864c34766931f71112e241ca16
Alex Skrypnyk <alex.designworks@gmail.com>
2019-10-24 11:20:53 +1100

    Removed mentions of Behat.

--- .circleci/process-artifacts.sh
+++ .circleci/process-artifacts.sh
@@ -9,9 +9,3 @@ if [ -d "$(pwd)/build/web/sites/simpletest/browser_output" ]; then
   mkdir -p /tmp/artifacts/simpletest
   cp -Rf "$(pwd)/build/web/sites/simpletest/browser_output/." /tmp/artifacts/simpletest
 fi
-
-if [ -d "$(pwd)/build/screenshots" ]; then
-  echo "==> Copying Behat test artifacts"
-  mkdir -p /tmp/artifacts/behat
-  cp -Rf "$(pwd)/build/screenshots" /tmp/artifacts/simpletest
-fi
