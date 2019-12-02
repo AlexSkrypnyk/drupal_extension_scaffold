ba21a219a6fa7555da0a40757c75bee25f14f435
Alex Skrypnyk <alex@integratedexperts.com>
2019-12-02 11:01:32 +1100

    Separated code linting into standalone script.

---
+++ .circleci/lint.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+##
+# Run lint checks.
+#
+set -e
+
+MODULE=$(basename -s .info.yml -- ./*.info.yml)
+
+echo "==> Lint code"
+build/vendor/bin/phpcs -s --standard=Drupal,DrupalPractice "build/web/modules/${MODULE}"
