8b8df4a68e9835109288d9126d1397ec8e5c977f
Alex Skrypnyk <alex@integratedexperts.com>
2019-12-05 23:39:49 +1100

    Added a prefix to the created database file.

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -2,6 +2,8 @@
 ##
 # Build.
 #
+# shellcheck disable=SC2015
+
 set -e
 
 echo "==> Validate composer"
@@ -23,17 +25,17 @@ netstat_opts='-tulpn'; [ "$(uname)" == "Darwin" ] && netstat_opts='-anv' || true
 netstat "${netstat_opts[@]}" | grep -q 8000 || (echo "ERROR: Unable to start inbuilt PHP server" && cat /tmp/php.log && exit 1)
 curl -s -o /dev/null -w "%{http_code}" -L -I http://localhost:8000 | grep -q 200 || (echo "ERROR: Server is started, but site cannot be served" && exit 1)
 
-echo "==> Install Drupal"
-build/vendor/bin/drush -r build/web si "${DRUPAL_PROFILE:-standard}" -y --db-url sqlite:///tmp/site.sqlite --account-name=admin install_configure_form.enable_update_status_module=NULL install_configure_form.enable_update_status_emails=NULL
-
 MODULE=$(basename -s .info.yml -- ./*.info.yml)
+
+echo "==> Install Drupal"
+build/vendor/bin/drush -r build/web si "${DRUPAL_PROFILE:-standard}" -y --db-url "sqlite:///tmp/site_${MODULE}.sqlite" --account-name=admin install_configure_form.enable_update_status_module=NULL install_configure_form.enable_update_status_emails=NULL
 
 echo "==> Symlink module code"
 rm -rf build/web/modules/"${MODULE}"/* > /dev/null
 mkdir -p "build/web/modules/${MODULE}"
 ln -s "$(pwd)"/* build/web/modules/"${MODULE}" && rm build/web/modules/"${MODULE}"/build
 
-echo "==> Enable module"
+echo "==> Enable module ${MODULE}"
 build/vendor/bin/drush -r build/web pm:enable "${MODULE}" -y
 build/vendor/bin/drush -r build/web cr
 build/vendor/bin/drush -r build/web -l http://localhost:8000 uli --no-browser
