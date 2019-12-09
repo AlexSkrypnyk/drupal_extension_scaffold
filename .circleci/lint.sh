a79a97487ec5a7a6aa8342428b9979f06fa64018
Alex Skrypnyk <alex@integratedexperts.com>
2019-12-09 19:53:26 +1100

    Added extensions for code linting.

--- .circleci/lint.sh
+++ .circleci/lint.sh
@@ -8,4 +8,4 @@ set -e
 MODULE=$(basename -s .info.yml -- ./*.info.yml)
 
 echo "==> Lint code"
-build/vendor/bin/phpcs -s --standard=Drupal,DrupalPractice "build/web/modules/${MODULE}"
+build/vendor/bin/phpcs -s --standard=Drupal,DrupalPractice --extensions=module,php,install,inc,test,info.yml,js "build/web/modules/${MODULE}"
