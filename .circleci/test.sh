4a1d2a67b6a8cb932f2b9c8bb746bbbe4cc482b5
Alex Skrypnyk <alex@integratedexperts.com>
2021-07-28 14:33:40 +1000

    Updated CI config.

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -5,17 +5,42 @@
 
 set -e
 
-MODULE=$(basename -s .info -- ./*.info)
+#-------------------------------------------------------------------------------
+# Variables (passed from environment; provided for reference only).
+#-------------------------------------------------------------------------------
+
+# Directory where Drupal site will be built.
+BUILD_DIR="${BUILD_DIR:-build}"
+
+# Webserver hostname.
+WEBSERVER_HOST="${WEBSERVER_HOST:-localhost}"
+
+# Webserver port.
+WEBSERVER_PORT="${WEBSERVER_PORT:-8000}"
+
+# Module name, taken from .info file.
+MODULE="$(basename -s .info -- ./*.info)"
+
+# Directory to store test results.
+TEST_RESULTS_DIR="${TEST_RESULTS_DIR:-/tmp/test_results/simpletest}"
+
+#-------------------------------------------------------------------------------
 
 echo "==> Run tests"
-[ ! -d tests ] && echo "==> No tests found. Skipping." && exit 0
 
-mkdir -p /tmp/test_results/simpletest
+# Do not fail if there are no tests.
+[ ! -d "tests" ] && echo "==> No tests were found. Skipping." && exit 0
+
+# Re-create test results directory.
+rm -rf "${TEST_RESULTS_DIR}" > /dev/null
+mkdir -p "${TEST_RESULTS_DIR}"
+
+# Remove existing test DB file.
 rm -f /tmp/test.sqlite
 
-php ./build/web/scripts/run-tests.sh \
-  --url http://localhost:8000 \
-  --xml /tmp/test_results/simpletest \
+php "./${BUILD_DIR}/web/scripts/run-tests.sh" \
+  --url "http://${WEBSERVER_HOST}:${WEBSERVER_PORT}" \
+  --xml "${TEST_RESULTS_DIR}" \
   --color \
   --verbose \
   --directory "sites/all/modules/${MODULE}"
