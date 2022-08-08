b7c01c0ef7faa755ff8d799d89389081d69d2a43
Alex Skrypnyk <alex@integratedexperts.com>
2022-08-08 18:32:18 +1000

    Updated example tests names.

--- tests/src/Unit/ExampleUnitTest.php
+++ tests/src/Unit/ExampleUnitTest.php
@@ -5,13 +5,13 @@ namespace Drupal\Tests\drupal_circleci_example\Unit;
 use Drupal\Tests\UnitTestCase;
 
 /**
- * Class DemoUnitTest.
+ * Class ExampleUnitTest.
  *
  * Demo test class to ensure that tests are discovered and can be ran.
  *
  * @group drupal_circleci_example
  */
-class DemoUnitTest extends UnitTestCase {
+class ExampleUnitTest extends UnitTestCase {
 
   /**
    * Test that unit tests are working.
