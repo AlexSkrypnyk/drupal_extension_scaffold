2964949230890df23eb5cc7337bbfe9e8fa65072
tan.nguyen <raucaixanh04@gmail.com>
2024-01-28 09:20:26 +0700

    Setup CI.

--- .devtools/deploy.sh
+++ .devtools/deploy.sh
@@ -3,7 +3,6 @@
 # Deploy code to a remote repository.
 #
 # - configures local git
-# - adds deployment SSH key to SSH agent
 # - force-pushes code to a remote code repository branch
 #
 # It is a good practice to create a separate Deployer user with own SSH key for
@@ -34,10 +33,6 @@ DEPLOY_REMOTE="${DEPLOY_REMOTE:-}"
 # Git branch to deploy. If not provided - current branch will be used.
 DEPLOY_BRANCH="${DEPLOY_BRANCH:-}"
 
-# The fingerprint of the SSH key of the user on behalf of which the deployment
-# is performed.
-DEPLOY_SSH_FINGERPRINT="${DEPLOY_SSH_FINGERPRINT:-}"
-
 # Set to 1 if the deployment should proceed. Useful for testing CI configuration
 # before an actual code push.
 DEPLOY_PROCEED="${DEPLOY_PROCEED:-0}"
@@ -51,19 +46,8 @@ echo "-------------------------------"
 [ -z "${DEPLOY_USER_NAME}" ] && echo "ERROR: Missing required value for DEPLOY_USER_NAME" && exit 1
 [ -z "${DEPLOY_USER_EMAIL}" ] && echo "ERROR: Missing required value for DEPLOY_USER_EMAIL" && exit 1
 [ -z "${DEPLOY_REMOTE}" ] && echo "ERROR: Missing required value for DEPLOY_REMOTE" && exit 1
-[ -z "${DEPLOY_SSH_FINGERPRINT}" ] && echo "ERROR: Missing required value for DEPLOY_SSH_FINGERPRINT" && exit 1
 
 [ "${DEPLOY_PROCEED}" != "1" ] && echo "> Skip deployment because $DEPLOY_PROCEED is not set to 1" && exit 0
-
-echo "> Configure git and SSH to connect to remote servers for deployment."
-mkdir -p "${HOME}/.ssh/"
-echo -e "Host *\n\tStrictHostKeyChecking no\n" >"${HOME}/.ssh/config"
-DEPLOY_SSH_FILE="${DEPLOY_SSH_FINGERPRINT//:/}"
-DEPLOY_SSH_FILE="${HOME}/.ssh/id_rsa_${DEPLOY_SSH_FILE//\"/}"
-[ ! -f "${DEPLOY_SSH_FILE:-}" ] && echo "ERROR: Unable to find Deploy SSH key file ${DEPLOY_SSH_FILE}." && exit 1
-if [ -z "${SSH_AGENT_PID:-}" ]; then eval "$(ssh-agent)"; fi
-ssh-add -D >/dev/null
-ssh-add "${DEPLOY_SSH_FILE}"
 
 echo "> Configure git user name and email, but only if not already set."
 [ "$(git config --global user.name)" == "" ] && echo "> Configure global git user name ${DEPLOY_USER_NAME}." && git config --global user.name "${DEPLOY_USER_NAME}"
