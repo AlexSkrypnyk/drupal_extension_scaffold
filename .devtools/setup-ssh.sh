3c77693c3ef1fd16edc4672ae07096431c436f9d
Alex Skrypnyk <alex@drevops.com>
2024-01-31 16:54:01 +0900

    Updated SSH fingerprint key variable.

--- .devtools/setup-ssh.sh
+++ .devtools/setup-ssh.sh
@@ -2,38 +2,45 @@
 ##
 # Setup SSH.
 #
-# - adds deployment SSH key to SSH agent
+# - adds the SSH key to the SSH agent
 #
-# It is a good practice to create a separate Deployer user with own SSH key for
+# It is a good practice to create a separate user with own SSH key for
 # every project.
 
 set -eu
 [ -n "${DEBUG:-}" ] && set -x
 
-# The fingerprint of the SSH key of the user on behalf of which the deployment
-# is performed.
-DEPLOY_SSH_FINGERPRINT="${DEPLOY_SSH_FINGERPRINT:-}"
+# The fingerprint of the SSH key.
+SSH_KEY_FINGERPRINT="${SSH_KEY_FINGERPRINT:-}"
 
-[ -z "${DEPLOY_SSH_FINGERPRINT}" ] && echo "ERROR: Missing required value for DEPLOY_SSH_FINGERPRINT" && exit 1
+#-------------------------------------------------------------------------------
 
-# echo "> Configure SSH to connect to remote servers for deployment."
-ls -al "${HOME}/.ssh/"
+[ -z "${SSH_KEY_FINGERPRINT}" ] && echo "ERROR: Missing required value for SSH_FINGERPRINT" && exit 1
+
 mkdir -p "${HOME}/.ssh/"
+
 echo -e "Host *\n\tStrictHostKeyChecking no\n" >"${HOME}/.ssh/config"
-DEPLOY_SSH_FILE="${DEPLOY_SSH_FINGERPRINT//:/}"
-DEPLOY_SSH_FILE="${HOME}/.ssh/id_rsa"
-[ ! -f "${DEPLOY_SSH_FILE:-}" ] && echo "ERROR: Unable to find Deploy SSH key file ${DEPLOY_SSH_FILE}." && exit 1
-if [ -z "${SSH_AGENT_PID:-}" ]; then eval "$(ssh-agent)"; fi
+
+file="${SSH_KEY_FINGERPRINT//:/}"
+file="${HOME}/.ssh/id_rsa_${file//\"/}"
+
+if [ ! -f "${file:-}" ]; then
+  echo "ERROR: Unable to find SSH key file ${file}."
+  exit 1
+fi
+
+if [ -z "${SSH_AGENT_PID:-}" ]; then
+  eval "$(ssh-agent)";
+fi
+
 ssh-add -D >/dev/null
-ssh-add "${DEPLOY_SSH_FILE}"
+ssh-add "${file}"
 
 echo "-------------------------------"
-echo "        Configure SSH          "
+echo "          Setup SSH            "
 echo "-------------------------------"
 echo
-echo "DEPLOY SSH FILE         : ${DEPLOY_SSH_FILE}"
-echo "DEPLOY SSH FINGERPRINT  : ${DEPLOY_SSH_FINGERPRINT}"
-echo
-echo "> Next steps:"
-echo "  .devtools/deploy.sh    # Deploy to remote"
-echo
+echo "SSH file          : ${file}"
+echo "SSH fingerprint   : ${SSH_KEY_FINGERPRINT}"
+echo "Loaded identities :"
+ssh-add -l
