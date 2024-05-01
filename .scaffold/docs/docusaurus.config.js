bb75c2603a8585ef8b5e67181e2cebb46759dcaa
tan.nguyen <raucaixanh04@gmail.com>
2024-05-01 15:21:42 +0700

    Test.

--- .scaffold/docs/docusaurus.config.js
+++ .scaffold/docs/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://alexSkrypnyk.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/drupal_extension_scaffold/',
 
   // GitHub pages deployment config.
   organizationName: 'AlexSkrypnyk',
