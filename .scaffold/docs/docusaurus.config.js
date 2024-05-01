af29d6ee232f62ec77d4b3a2e325be856d5cf0e5
tan.nguyen <raucaixanh04@gmail.com>
2024-05-01 15:04:21 +0700

    Init GHA.

--- .scaffold/docs/docusaurus.config.js
+++ .scaffold/docs/docusaurus.config.js
@@ -8,12 +8,12 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Scaffold',
-  tagline: 'Scaffold documentation',
+  title: 'Drupal extension scaffold',
+  tagline: 'Drupal extension scaffold documentation',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'http://localhost:8080',
+  url: 'https://alexSkrypnyk.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
