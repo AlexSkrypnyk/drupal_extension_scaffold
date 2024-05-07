d0709ff20733f54608d22adaca165259ba6a4f97
Alex Skrypnyk <alex@drevops.com>
2024-05-07 12:38:00 +1000

    Added `ahoy stop` command and enhanced output.

--- .devtools/assemble.sh
+++ .devtools/assemble.sh
@@ -37,6 +37,15 @@ DRUPAL_PROJECT_REPO="${DRUPAL_PROJECT_REPO:-https://github.com/drupal-composer/d
 
 #-------------------------------------------------------------------------------
 
+# @formatter:off
+note() { printf "       %s\n" "${1}"; }
+info() { [ "${TERM:-}" != "dumb" ] && tput colors >/dev/null 2>&1 && printf "\033[34m[INFO] %s\033[0m\n" "${1}" || printf "[INFO] %s\n" "${1}"; }
+pass() { [ "${TERM:-}" != "dumb" ] && tput colors >/dev/null 2>&1 && printf "\033[32m[ OK ] %s\033[0m\n" "${1}" || printf "[ OK ] %s\n" "${1}"; }
+fail() { [ "${TERM:-}" != "dumb" ] && tput colors >/dev/null 2>&1 && printf "\033[31m[FAIL] %s\033[0m\n" "${1}" || printf "[FAIL] %s\n" "${1}"; }
+# @formatter:on
+
+#-------------------------------------------------------------------------------
+
 echo "==============================="
 echo "         🏗️ ASSEMBLE           "
 echo "==============================="
@@ -45,11 +54,12 @@ echo
 # Make sure Composer doesn't run out of memory.
 export COMPOSER_MEMORY_LIMIT=-1
 
-echo "> Validate tools."
+info "Validate tools."
 ! command -v git >/dev/null && echo "ERROR: Git is required for this script to run." && exit 1
 ! command -v php >/dev/null && echo "ERROR: PHP is required for this script to run." && exit 1
 ! command -v composer >/dev/null && echo "ERROR: Composer (https://getcomposer.org/) is required for this script to run." && exit 1
 ! command -v jq >/dev/null && echo "ERROR: jq (https://stedolan.github.io/jq/) is required for this script to run." && exit 1
+pass "Tools are valid."
 
 # Extension name, taken from the .info file.
 extension="$(basename -s .info.yml -- ./*.info.yml)"
@@ -58,77 +68,95 @@ extension="$(basename -s .info.yml -- ./*.info.yml)"
 # Extension type.
 type=$(grep -q "type: theme" "${extension}.info.yml" && echo "themes" || echo "modules")
 
-echo "> Validate Composer configuration."
+info "Validate Composer configuration."
 composer validate --ansi --strict
 
 # Reset the environment.
-[ -d "build" ] && echo "> Remove existing build directory." && chmod -Rf 777 "build" && rm -rf "build"
+if [ -d "build" ]; then
+  info "Removing existing build directory."
+  chmod -Rf 777 "build" >/dev/null || true
+  rm -rf "build" >/dev/null || true
+  pass "Existing build directory removed."
+fi
 
+info "Creating Drupal codebase."
 # Allow installing custom version of Drupal core from drupal-composer/drupal-project,
 # but only coupled with drupal-project SHA (required to get correct dependencies).
 if [ -n "${DRUPAL_VERSION:-}" ] && [ -n "${DRUPAL_PROJECT_SHA:-}" ]; then
-  echo "> Initialise Drupal site from the scaffold repo ${DRUPAL_PROJECT_REPO} commit ${DRUPAL_PROJECT_SHA}."
+  note "Initialising Drupal site from the scaffold repo ${DRUPAL_PROJECT_REPO} commit ${DRUPAL_PROJECT_SHA}."
 
   # Clone Drupal core at the specific commit SHA.
   git clone -n "${DRUPAL_PROJECT_REPO}" "build"
   git --git-dir="build/.git" --work-tree="build" checkout "${DRUPAL_PROJECT_SHA}"
   rm -rf "build/.git" >/dev/null
 
-  echo "> Pin Drupal to a specific version ${DRUPAL_VERSION}."
+  note "Pin Drupal to a specific version ${DRUPAL_VERSION}."
   sed_opts=(-i) && [ "$(uname)" == "Darwin" ] && sed_opts=(-i '')
   sed "${sed_opts[@]}" 's|\(.*"drupal\/core"\): "\(.*\)",.*|\1: '"\"$DRUPAL_VERSION\",|" "build/composer.json"
   cat "build/composer.json"
 else
-  echo "> Initialise Drupal site from the latest scaffold."
+  note "Initialising Drupal site from the latest scaffold."
   # There are no releases in "drupal-composer/drupal-project", so have to use "@dev".
   composer create-project drupal-composer/drupal-project:@dev "build" --no-interaction --no-install
 fi
+pass "Drupal codebase created."
 
-echo "> Merge configuration from composer.dev.json."
+info "Merging configuration from composer.dev.json."
 php -r "echo json_encode(array_replace_recursive(json_decode(file_get_contents('composer.dev.json'), true),json_decode(file_get_contents('build/composer.json'), true)),JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES);" >"build/composer2.json" && mv -f "build/composer2.json" "build/composer.json"
 
-echo "> Merge configuration from extension's composer.json."
+info "Merging configuration from extension's composer.json."
 php -r "echo json_encode(array_replace_recursive(json_decode(file_get_contents('composer.json'), true),json_decode(file_get_contents('build/composer.json'), true)),JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES);" >"build/composer2.json" && mv -f "build/composer2.json" "build/composer.json"
 
-echo "> Create GitHub authentication token if provided."
-[ -n "${GITHUB_TOKEN:-}" ] && composer config --global github-oauth.github.com "${GITHUB_TOKEN}" && echo "Token: " && composer config --global github-oauth.github.com
+if [ -n "${GITHUB_TOKEN:-}" ]; then
+  info "Adding GitHub authentication token if provided."
+  composer config --global github-oauth.github.com "${GITHUB_TOKEN}"
+  composer config --global github-oauth.github.com | grep -q "gh" || fail "GitHub token not added."
+  pass "GitHub token added."
+fi
 
-echo "> Create custom directories."
+info "Creating custom directories."
 mkdir -p build/web/modules/custom build/web/themes/custom
 
-echo "> Install dependencies."
+info "Installing dependencies."
 composer --working-dir="build" install
+pass "Dependencies installed."
 
 # Suggested dependencies allow to install them for testing without requiring
 # them in extension's composer.json.
-echo "> Install suggested dependencies from extension's composer.json."
+info "Installing suggested dependencies from extension's composer.json."
 composer_suggests=$(cat composer.json | jq -r 'select(.suggest != null) | .suggest | keys[]')
 for composer_suggest in $composer_suggests; do
   composer --working-dir="build" require "${composer_suggest}"
 done
+pass "Suggested dependencies installed."
 
-echo "> Copy tools configuration files."
+info "Copying tools configuration files."
 cp phpcs.xml phpstan.neon phpmd.xml rector.php .twig-cs-fixer.php phpunit.xml "build/"
+pass "Tools configuration files copied."
 
-echo "> Symlink extension's code."
+info "Symlinking extension's code."
 rm -rf "build/web/${type}/custom" >/dev/null && mkdir -p "build/web/${type}/custom/${extension}"
 ln -s "$(pwd)"/* "build/web/${type}/custom/${extension}" && rm "build/web/${type}/custom/${extension}/build"
+pass "Extension's code symlinked."
 
 # If front-end dependencies are used in the project, package-lock.json is
 # expected to be committed to the repository.
 if [ -f "build/web/${type}/custom/${extension}/package-lock.json" ]; then
   pushd "build/web/${type}/custom/${extension}" >/dev/null || exit 1
-  echo "> Install front-end dependencies."
+
+  info "Installing front-end dependencies."
   if [ -f ".nvmrc" ]; then nvm use; fi
   if [ ! -d "node_modules" ]; then npm ci; fi
   echo "> Build front-end dependencies."
   npm run build
+  pass "Front-end dependencies installed."
+
   popd >/dev/null || exit 1
 fi
 
 echo
 echo "==============================="
-echo "     🏗 ASSEMBLE COMPLETE      "
+echo "    🏗 ASSEMBLE COMPLETE ✅   "
 echo "==============================="
 echo
 echo "> Next steps:"
