cdffecc9fad4c7ad7cdd6aade4c760333d2541aa
tan.nguyen <raucaixanh04@gmail.com>
2024-05-10 11:50:21 +0700

    Init internal replacement.

--- .scaffold/Customizer.php
+++ .scaffold/Customizer.php
@@ -8,6 +8,7 @@ use Composer\IO\IOInterface;
 use Composer\Script\Event;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Filesystem\Path;
+use Symfony\Component\Finder\Finder;
 
 class Customizer {
 
@@ -71,20 +72,21 @@ class Customizer {
    * Display summary input.
    */
   protected function displaySummary(): void {
-    $this->io->write('            Summary');
-    $this->io->write('---------------------------------');
-    $this->io->write("Name                             : {$this->extensionName}");
-    $this->io->write("Machine name                     : {$this->extensionMachineName}");
-    $this->io->write("Type                             : {$this->extensionType}");
-    $this->io->write("CI Provider                      : {$this->ciProvider}");
-    $this->io->write("Command wrapper                  : {$this->commandWrapper}");
-    $this->io->write("Working dir                      : {$this->workingDir}");
+    $this->io->notice('            Summary');
+    $this->io->notice('---------------------------------');
+    $this->io->notice("Name                             : {$this->extensionName}");
+    $this->io->notice("Machine name                     : {$this->extensionMachineName}");
+    $this->io->notice("Type                             : {$this->extensionType}");
+    $this->io->notice("CI Provider                      : {$this->ciProvider}");
+    $this->io->notice("Command wrapper                  : {$this->commandWrapper}");
+    $this->io->notice("Working dir                      : {$this->workingDir}");
   }
 
   /**
    * Process README.md.
    */
   protected function processReadme(): void {
+    $this->io->notice('Processing README.');
     $this->fileSystem->remove("$this->workingDir/README.md");
     $this->fileSystem->rename("$this->workingDir/README.dist.md", "$this->workingDir/README.md");
     $logo_url = sprintf(
@@ -100,15 +102,49 @@ class Customizer {
 
   /**
    * Internal process to replace scaffold string and remove scaffold files.
+   * @throws \Exception
    */
   protected function processInternalReplacement(): void {
+    $this->io->notice('Processing internal replacement.');
 
+    $extension_machine_name_class = self::convertString($this->extensionMachineName, 'class_name');
+
+    self::replaceStringInFilesInDirectory('YourNamespace', $this->extensionMachineName, $this->workingDir);
+    self::replaceStringInFilesInDirectory('yournamespace', $this->extensionMachineName, $this->workingDir);
+    self::replaceStringInFilesInDirectory('AlexSkrypnyk', $this->extensionMachineName, $this->workingDir);
+    self::replaceStringInFilesInDirectory('alexskrypnyk', $this->extensionMachineName, $this->workingDir);
+    self::replaceStringInFilesInDirectory('yourproject', $this->extensionMachineName, $this->workingDir);
+    self::replaceStringInFilesInDirectory('Your+Extension', $this->extensionMachineName, $this->workingDir);
+    self::replaceStringInFilesInDirectory('your_extension', $this->extensionMachineName, $this->workingDir);
+    self::replaceStringInFilesInDirectory('drupal_extension_scaffold', $this->extensionMachineName, $this->workingDir);
+    self::replaceStringInFilesInDirectory('[EXTENSION_NAME]', $this->extensionMachineName, $this->workingDir);
+    self::replaceStringInFilesInDirectory(
+      'Provides your_extension functionality.',
+      "Provides $this->extensionMachineName functionality.",
+      $this->workingDir,
+    );
+    self::replaceStringInFilesInDirectory(
+      'Drupal module scaffold FE example used for template testing',
+      "Provides $this->extensionMachineName functionality.",
+      $this->workingDir,
+    );
+
+    self::replaceStringInFilesInDirectory('Drupal extension scaffold', $this->extensionName, $this->workingDir);
+    self::replaceStringInFilesInDirectory('Yourproject', $this->extensionName, $this->workingDir);
+    self::replaceStringInFilesInDirectory('Your Extension', $this->extensionName, $this->workingDir);
+    self::replaceStringInFilesInDirectory('your extension', $this->extensionName, $this->workingDir);
+
+    self::replaceStringInFilesInDirectory('drupal-module', "drupal-$this->extensionType", $this->workingDir);
+    self::replaceStringInFilesInDirectory('type: module', "type: $this->extensionType", $this->workingDir);
+
+    self::replaceStringInFilesInDirectory('YourExtension', $extension_machine_name_class, $this->workingDir);
   }
 
   /**
    * Remove CI provider depends on CI provider selected.
    */
   protected function removeCiProvider(): void {
+    $this->io->notice('Processing remove CI Provider.');
     $ci_provider = $this->ciProvider;
     if ($ci_provider === 'gha') {
       $this->removeCircleciCiProvider();
@@ -135,6 +171,7 @@ class Customizer {
    * Remove command wrappers depends on command wrapper selected.
    */
   protected function removeCommandWrapper(): void {
+    $this->io->notice('Processing remove Command Wrapper.');
     $command_wrapper = $this->commandWrapper;
     switch ($command_wrapper) {
       case 'ahoy':
@@ -170,13 +207,31 @@ class Customizer {
   public static function main(Event $event): void {
     $io = $event->getIO();
     $extension_name = $io->ask('Name: ', 'Your Extension');
-    $extension_machine_name = $io->ask('Machine Name: ', 'your_extension');
-    $extension_type = 'module';
-    $extension_type = $io->ask('Type: module or theme: ', $extension_type);
-    $ci_provider = 'gha';
-    $ci_provider = $io->ask('CI Provider: GitHub Actions (gha) or CircleCI (circleci): ', $ci_provider);
-    $command_wrapper = 'ahoy';
-    $command_wrapper = $io->ask('Command wrapper: Ahoy (ahoy), Makefile (makefile), None (none): ', $command_wrapper);
+    $default_extension_machine_name = self::convertString($extension_name, 'file_name');
+    $extension_machine_name = $io->ask("Machine Name: [$default_extension_machine_name]", $default_extension_machine_name);
+    $default_extension_type = 'module';
+    $extension_type = $io->ask("Type: module or theme: [$default_extension_type]", $default_extension_type);
+    $default_ci_provider = 'gha';
+    $ci_provider = $io->ask("CI Provider: GitHub Actions (gha) or CircleCI (circleci): [$default_ci_provider]", $default_ci_provider);
+    $default_command_wrapper = 'ahoy';
+    $command_wrapper = $io->ask("Command wrapper: Ahoy (ahoy), Makefile (makefile), None (none): [$default_command_wrapper]", $default_command_wrapper);
+
+    if (!$extension_name) {
+      throw new \Exception('Name is required.');
+    }
+    if (!$extension_machine_name) {
+      throw new \Exception('Machine name is required.');
+    }
+    if (!in_array($extension_type, ['theme', 'module'])) {
+      throw new \Exception('Extension type is required or invalid.');
+    }
+    if (!in_array($ci_provider, ['gha', 'circleci'])) {
+      throw new \Exception('CI provider is required or invalid.');
+    }
+    if (!in_array($command_wrapper, ['ahoy', 'makefile', 'none'])) {
+      throw new \Exception('Command wrapper is required or invalid.');
+    }
+
     $working_dir = Path::makeAbsolute('..', __DIR__);
     $fileSystem = new Filesystem();
     $customizer = new static(
@@ -243,5 +298,30 @@ class Customizer {
     }
 
     return $string_out;
+  }
+
+  /**
+   * Replace string in files in a directory.
+   *
+   * @param string $string_search
+   *   String to search.
+   * @param string $string_replace
+   *   String to replace.
+   * @param string $directory
+   *   Directory.
+   */
+  public static function replaceStringInFilesInDirectory(string $string_search, string $string_replace, string $directory): void {
+    $finder = new Finder();
+    $finder
+      ->files()
+      ->contains($string_search)
+      ->in($directory);
+    if ($finder->hasResults()) {
+      foreach ($finder as $file) {
+        $file_content = $file->getContents();
+        $new_file_content = str_replace($string_search, $string_replace, $file_content);
+        file_put_contents($file->getRealPath(), $new_file_content);
+      }
+    }
   }
 }
