d16d4eeb59fdbe9b671e3c744771cec748afd013
Alex Skrypnyk <alex@drevops.com>
2024-06-28 11:29:08 +1000

    Removed asset packagist from the assembled code by default.

--- .devtools/assemble.sh
+++ .devtools/assemble.sh
@@ -107,6 +107,11 @@ php -r "echo json_encode(array_replace_recursive(json_decode(file_get_contents('
 info "Merging configuration from extension's composer.json."
 php -r "echo json_encode(array_replace_recursive(json_decode(file_get_contents('composer.json'), true),json_decode(file_get_contents('build/composer.json'), true)),JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES);" >"build/composer2.json" && mv -f "build/composer2.json" "build/composer.json"
 
+# Asset Packagist sometimes fails, so we remove it by default. If it's needed,
+# the lines below can be commented out.
+info "Remove asset-packagist"
+composer --working-dir="build" config --unset repositories.1 || true
+
 if [ -d "patches" ]; then
   info "Copying patches."
   mkdir -p "build/patches"
